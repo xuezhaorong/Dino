f93681d47385873c6dc4cf2d352843963fafbe6e
XueSakuraEnd <92241022+XueSakuraEnd@users.noreply.github.com>
2023-05-10 21:16:24 +0800

    1

---
+++ interface/Restart.py
@@ -0,0 +1,11 @@
+import pygame
+# 重启按钮
+class RestartInterface():
+    def __init__(self,restartImage):
+        self.image = restartImage
+        self.rect = self.image.get_rect()
+        self.rect.x = 420
+        self.rect.y = 160
+    
+    def draw(self,windowSurface):
+        windowSurface.blit(self.image,self.rect)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from interface.Background import Background
 from interface.Dinosaur import Dinosaur
 from interface.Score import ScoreInterface
+from interface.Restart import RestartInterface
 
 # 全局初始化
 pygame.init()
@@ -24,6 +25,10 @@ image_list = []
 for i in range(1, 7):
     image = pygame.image.load(f"./material/picture/Dino{i}.png")
     image_list.append(image)
+# 加载失败图
+failImage = pygame.image.load("./material/picture/gameover.png")
+# 加载重启按钮图
+restartImage = pygame.image.load("./material/picture/Restart.png")
 
 # 背景音乐
 pygame.mixer.music.load("./material/sound/背景音效2.mp3")
@@ -32,13 +37,16 @@ pygame.mixer.music.set_volume(0.4)
 
 # 加载音效
 jumpSound = pygame.mixer.Sound("./material/sound/跳跃音效.mp3")
+failSound = pygame.mixer.Sound("./material/sound/失败音效.mp3")
 
 # 加载字体
-textFont = pygame.font.Font("./material/MaoKenWangXingYuan-2.ttf",15)
+textFont = pygame.font.Font("./material/MaoKenWangXingYuan-2.ttf", 15)
 
 # 初始化分数
 score = 0
 # 初始化时间
+currentTime = 1000
+# 动态时间
 time = 1000
 
 # 创建时钟对象
@@ -50,9 +58,26 @@ background = Background(bgSurface)
 dinosaur = Dinosaur(image_list)
 # 创建字体对象
 scoreInterface = ScoreInterface(textFont)
+# 创建重启按钮对象
+restartInterface = RestartInterface(restartImage)
+
+# 失败标志
+fail = False
+
+# 重开函数
+
+
+def restart():
+    global score, time
+    score = 0
+    time = currentTime
+
 
 if __name__ == "__main__":
-    
+
+    RESET_TICK_EVENT = pygame.USEREVENT + 1
+    pygame.time.set_timer(RESET_TICK_EVENT, 0)
+
     # 开启消息循环
     while True:
         # 事件处理
@@ -61,27 +86,47 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            # 处理键盘事件 
-            elif event.type == pygame.KEYDOWN:
+            # 处理键盘事件
+            elif event.type == pygame.KEYDOWN and not fail:
                 if event.key == pygame.K_SPACE and dinosaur.rect.y == 240:
                     # 绘制恐龙
                     dinosaur.UP()
                     # 发出音效
                     jumpSound.play()
+                    # 处理鼠标事件
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if restartInterface.rect.collidepoint(event.pos):
+                    fail = False
+                    restart()
+        if not fail:
+            # 绘制背景
+            background.draw(windowSurface)
 
-        # 绘制背景
-        background.draw(windowSurface)
+            # 绘制恐龙
+            dinosaur.draw(windowSurface)
 
-        # 绘制恐龙
-        dinosaur.draw(windowSurface)
+            # 绘制分数
+            t = pygame.time.get_ticks()
+            if t > time:
+                score += 1
+                time += 1000
+            scoreInterface.draw(windowSurface, f"Score: {score}")
 
-        # 绘制分数
-        # 获取时间
-        t = pygame.time.get_ticks() 
-        if t > time:
-            score += 1
-            time += 1000
-        scoreInterface.draw(windowSurface,f"Score: {score}")
+        if time == 10000:
+            fail = True
+
+        # 计时器
+        t = pygame.time.get_ticks()
+        if t > currentTime:
+            currentTime += 1000
+
+        if fail:
+            # 失败音效
+            failSound.play()
+            # 绘制失败图片
+            windowSurface.blit(failImage, (260, 100))
+            # 绘制重启按钮
+            restartInterface.draw(windowSurface)
 
         # 刷新界面
         pygame.display.flip()
