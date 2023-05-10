7849cd43010658c824588194fe5b414a98ec5f80
XueSakuraEnd <92241022+XueSakuraEnd@users.noreply.github.com>
2023-05-10 13:11:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ import pygame
 pygame.init()
 #音效初始化
 pygame.mixer.init()
+#设置窗口大小
+resolution = width,height = 900,300
+#设置全局分辨率
+windowSurface = pygame.display.set_mode(resolution)
+#设置标题
+pygame.display.set_caption("Dino")
+#设置图标
+
+
+
+
+
+
+
+
