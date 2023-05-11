7f2ab16ec8983b06be4dc9fc9dd9d3af9b329d3e
XueSakuraEnd <92241022+XueSakuraEnd@users.noreply.github.com>
2023-05-11 20:44:40 +0800

    Delete interface directory

--- interface/Score.py
+++
@@ -1,9 +0,0 @@
-import pygame
-# 分数类
-class ScoreInterface():
-    def __init__(self,textFont):
-        self.textFont = textFont
-    
-    def draw(self,windowSurface,score):
-        textSurface = self.textFont.render(score,True,(64,64,64))
-        windowSurface.blit(textSurface,(10,10))
